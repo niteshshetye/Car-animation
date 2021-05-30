db3992115d25c0bb9cbf0096a397a7a634bedff4
Nitesh <niteshshetye204@gmail.com>
2021-05-30 12:57:18 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+console.log('Car Animated Project');
+
+var audio = document.createElement('audio');
+audio.setAttribute('src', 'sound.mp3');
+// audio.setAttribute('allow', 'autoplay');
+audio.loop = true;
+// audio.play();
+
+// Audio part is remaning some policies are changes it can be solve later
